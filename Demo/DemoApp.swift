9e99665fa09fb60178a370c409f1da1c667485f5
ChengshengDeng <dylanden@bu.edu>
2022-11-27 21:22:31 +0800

    Initial Commit

---
+++ Demo/DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApp.swift
+//  Demo
+//
+//  Created by Chengsheng Deng on 2022/11/27.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
